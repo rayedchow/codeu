b3808932fd83e490a5dde78d09e63061239da6bc
aaronbiscotti <thesoupmanguy@gmail.com>
2022-09-24 12:06:21 -0400

    dfw

--- codeu/src/App.tsx
+++ codeu/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactS
+          Learn Reactf2wefw
         </a>
       </header>
     </div>
