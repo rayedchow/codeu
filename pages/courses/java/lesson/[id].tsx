ecb42154b64045a04a08c592bab2621d3ea8066b
voomp <bat7gamer7@gmail.com>
2022-09-25 07:40:28 -0400

    unit 1 json

--- pages/courses/java/lesson/[id].tsx
+++ pages/courses/java/lesson/[id].tsx
@@ -1,5 +1,5 @@
-import Content from '../../../components/Content'
-import MCQuestion from '../../../components/MCQuestion'
+import Content from '../../../../components/Content'
+import MCQuestion from '../../../../components/MCQuestion'
 
 const Question = () => {
 	return (
@@ -19,24 +19,27 @@ const Question = () => {
 					</div>
 					<div className="mt-6 prose prose-indigo prose-lg mx-auto">
 					<p>
-						A type is a set of values (e.g. integers, floaters, etc.) and a set of operations (e.g. +, -, *, /, etc.) on them.
-						Data types can be categorized as either **primitive** or **reference**.
+						A type is a set of values (e.g. integers, floaters, etc.) and a set of operations (e.g. +, -, *, /, etc.) on them.<br /><br />
+						Data types can be categorized as either <b>primitive</b> or <b>reference</b>.<br /><br />
 
-						The primitive types used in this course define the set of operations for numbers and boolean (true/false) values.
-						Examples are:
+						The primitive types used in this course define the set of operations for numbers and boolean (true/false) values. <br />
+						Examples are:<br />
 					</p>
 					<ul role="list">
-						<li>int - stores integers (whole numbers like 5, 84, -49893</li>
-						<li>double - stores floating point numbers (decimal numbers like 6.3, -0.9, 60931.89479</li>
-						<li>boolean - stores Boolean values (either true or false)</li>
+						<li><b>int</b> - stores integers (whole numbers like 5, 84, -49893</li>
+						<li><b>double</b> - stores floating point numbers (decimal numbers like 6.3, -0.9, 60931.89479</li>
+						<li><b>boolean</b> - stores Boolean values (either true or false)</li>
 					</ul>
 					<p>
-						Here is a question that will test your knowledge on what was just said.
+						Here is a question that .
 					</p>
 					<MCQuestion number={1} title="Primitive types are..." romanOptions={["Primitive", "Reference types", "A data structure"]} options={["I only", "II and III", "III only", "None"]} correctAnswer="III only" />
 					<div className="bg-black text-white p-5 h-[200px]">
 						Code here
 					</div>
+					<br />
+					<button className="submit">Next lesson</button>
+					<p><i>All lessons are from <a href="https://longbaonguyen.github.io/courses/apcsa/apjava.html" target="_blank">https://longbaonguyen.github.io/courses/apcsa/apjava.html</a></i></p>
 					</div>
 				</div>
 				</div>
